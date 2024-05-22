5c83834dd7feb94e89149590157ff3afbaad8148
NintenHero <mchinrichs@gmail.com>
2024-05-22 12:00:10 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO.Compression;
+
+namespace Football_Forever_3_Decompressor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                BinaryReader br = new(File.OpenRead(args[i]));
+
+                br.BaseStream.Position = 8;
+                int size = br.ReadInt32();
+                br.BaseStream.Position = 50;
+
+                using (var ds = new DeflateStream(new MemoryStream(br.ReadBytes(size)), CompressionMode.Decompress))
+                    ds.CopyTo(File.Create(Path.GetDirectoryName(args[i]) + "//" + Path.GetFileNameWithoutExtension(args[i])));
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Football-Forever-3-Decompressor
+Decompresses KOM files from the game Football Forever 3, AKA Kicks Online.
+Drag and drop as many as you want onto the application.
+
+Unfortunately, the game is no longer avalible for purchase on steam.
+If you have it in your library, you can still reinstall it with steam://install/1255970.
